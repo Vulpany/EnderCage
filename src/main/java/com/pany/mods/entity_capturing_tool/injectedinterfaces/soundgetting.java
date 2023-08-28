2096756ee10511698c388a8be3e463995575920c
Vulpany <114545114+Vulpany@users.noreply.github.com>
2023-08-29 00:40:24 +0200

    Delete src/main directory

--- src/main/java/com/pany/mods/entity_capturing_tool/injectedinterfaces/soundgetting.java
+++
@@ -1,9 +0,0 @@
-package com.pany.mods.entity_capturing_tool.injectedinterfaces;
-
-import net.minecraft.sound.SoundEvent;
-
-public interface soundgetting {
-    public default SoundEvent GetAmbientPublic() {
-        return null;
-    }
-}
